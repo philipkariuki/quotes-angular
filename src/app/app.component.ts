0060b4e9cbc81cb11c05a43c4d706320dcdf8cb8
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-02 22:36:37 +0300

    Updates quotes.component.ts and app.component.ts
    -Moves array of quotes from app.component.ts to quotes.component.ts
    -Imports /quotes to quotes.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,7 @@ import { Quotes } from './quotes';
 })
 export class AppComponent {
   title = 'Quotes';
-  quote = [
-    new Quotes(1,'This is a test quote'),
-    new Quotes(2,'Another test quote'),
-    new Quotes(3, 'Yet another test quote'),
-    new Quotes(4, 'Who doesn;t love quotes'),
-    new Quotes(5, 'I could do this all day'),
 
-]
 }
 
 
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from './quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  quote = [
+    new Quotes(1,'This is a test quote'),
+    new Quotes(2,'Another test quote'),
+    new Quotes(3, 'Yet another test quote'),
+    new Quotes(4, 'Who doesn;t love quotes'),
+    new Quotes(5, 'I could do this all day'),
+
+]
 
   constructor() { }
 
