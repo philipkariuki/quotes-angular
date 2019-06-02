445bc376a6b0976fdc5832f5eba963e4d2c55d32
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-03 01:37:28 +0300

    Update quotes.ts
    Updates function, sets showDescription to false.

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,3 +1,6 @@
 export class Quotes {
-  constructor(public id:number, public name:string, public author:string, public submitter:string){}
+  public showDescription:boolean;
+  constructor(public id:number, public name:string, public author:string, public submitter:string){
+  this.showDescription=false
+  }
 }
