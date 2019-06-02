731c058aebf8d4891c77a6fdeaf4498f7418f3bb
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-02 17:41:09 +0300

    Update app.component.ts
    -Imports Quote class
    -Creates an array of new instances of the Quote class
    -Removes previous constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quotes } from './quotes';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes';
-  quote: string[];
+  quote = [
+    new Quotes(1,'This is a test quote'),
+    new Quotes(2,'Another test quote'),
+    new Quotes(3, 'Yet another test quote'),
+    new Quotes(4, 'Who doesn;t love quotes'),
+    new Quotes(5, 'I could do this all day'),
 
-  constructor(){
-    this.quote = ["this is a test quote","another test quote","yet another test quote"];
-  }
+]
 }
+
+
+// quote: string[];
+
+//   constructor(){
+//     this.quote = ["this is a test quote","another test quote","yet another test quote"];
+//   }
