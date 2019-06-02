8db544fe4d405553cf946decd78da070caae249f
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-02 22:55:25 +0300

    Nesting components
    -Moves code from root app template to quotes.component.html replacing them with<app-quotes> selector tags.
    -Removes import quotes from app.component.ts.
    -Inserts code from root template to child component i.e. quotes.component.html.
    -Fixes file path to quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Quotes } from './quotes';
 
 @Component({
   selector: 'app-root',
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Quotes } from './quotes';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
