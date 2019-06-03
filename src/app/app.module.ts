dfb134aeb68906928934ce4f77e7073de9ddb737
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-03 05:34:26 +0300

    Update app.module.ts
    Imports FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
@@ -16,7 +17,8 @@ import { QuotesFormComponent } from './quotes-form/quotes-form.component';
     QuotesFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
