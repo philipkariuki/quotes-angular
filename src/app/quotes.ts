5adcc6daf58f44df5f1b9ff3cd03e618531d1f5b
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-03 09:15:46 +0300

    Auto-generated commit

--- src/app/quotes.ts
+++
@@ -1,6 +0,0 @@
-export class Quotes {
-  public showDescription:boolean;
-  constructor(public id:number, public name:string, public author:string, public submitter:string, public dateAdded:Date){
-  this.showDescription=false
-  }
-}
