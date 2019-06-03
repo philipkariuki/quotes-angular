824c4547eb9194eb8e5f5021b010c3a4b367d46b
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-03 05:38:44 +0300

    Update quotes-form.component.ts
    Imports from quotes

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes-form',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
+  newQuotes=new Quotes(0,"","",new Date());
 
   constructor() { }
 
