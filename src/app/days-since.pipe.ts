e8e0bf3d462f107d175515588917815ace51e129
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-03 04:45:59 +0300

    Major breakthrough
    Finally working custom pipe showing days since quote created

--- src/app/days-since.pipe.ts
+++ src/app/days-since.pipe.ts
@@ -5,8 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DaysSincePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
 
+
+  transform(value: any): number {
+    let today:Date = new Date(); //get current date and time
+    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    var timeDifference =Math.abs(value-todayWithNoTime )
+    var daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
+    return daysDifference;
+
+  }
 }
