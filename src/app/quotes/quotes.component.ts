22f8791dabe1f1ac445f409ca79158ee9f5c52db
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-03 03:01:07 +0300

    Update quotes.component.ts
    Adds Date property

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,13 +11,13 @@ export class QuotesComponent implements OnInit {
   title = 'Quotes';
 
   quotes = [
-    new Quotes(1, '"It Doesn’t Matter Where You Came From. All That Matters Is Where You Are Going."','Author - Brian Tracy','Submitted by: Phil'),
-    new Quotes(2, '"Think Big And Don’t Listen To People Who Tell You It Can’t Be Done. Life’s Too Short To Think Small."','Author - Tim Ferriss','Submitted by: Paul'),
-    new Quotes(3,  '“The Person Who Says It Cannot Be Done Should Not Interrupt The Person Who Is Doing It.”','Author - Chinese Proverb','Submitted by: Paula'),
-    new Quotes(4,  '“Everything You’ve Ever Wanted Is On The Other Side Of Fear.”','Author - George Addair','Submitted by: Georgina'),
-    new Quotes(5, '“The Only Place Where Success Comes Before Work Is In The Dictionary.”','Author - Vidal Sassoon','Submitted by: Naomi'),
-    new Quotes(6, '“All Our Dreams Can Come True If We Have The Courage To Pursue Them”','Author - Walt Disney','Submitted by: George'),
-    new Quotes(7, '"Never fear shadows. They simply mean there is a light shining somewhere nearby."','Author - Ruth Renkel','Submitted by: Max'),
+    new Quotes(1, '"It Doesn’t Matter Where You Came From. All That Matters Is Where You Are Going."','Author - Brian Tracy','Submitted by: Phil', new Date(2005,3,14) ),
+    new Quotes(2, '"Think Big And Don’t Listen To People Who Tell You It Can’t Be Done. Life’s Too Short To Think Small."','Author - Tim Ferriss','Submitted by: Paul' new Date(1991,2,25) ),
+    new Quotes(3,  '“The Person Who Says It Cannot Be Done Should Not Interrupt The Person Who Is Doing It.”','Author - Chinese Proverb','Submitted by: Paula' new Date(2011,7,7) ),
+    new Quotes(4,  '“Everything You’ve Ever Wanted Is On The Other Side Of Fear.”','Author - George Addair','Submitted by: Georgina' new Date(2017,6,17) ),
+    new Quotes(5, '“The Only Place Where Success Comes Before Work Is In The Dictionary.”','Author - Vidal Sassoon','Submitted by: Naomi' new Date(2018,0,29) ),
+    new Quotes(6, '“All Our Dreams Can Come True If We Have The Courage To Pursue Them”','Author - Walt Disney','Submitted by: George' new Date(2019,5,19) ),
+    new Quotes(7, '"Never fear shadows. They simply mean there is a light shining somewhere nearby."','Author - Ruth Renkel','Submitted by: Max' new Date(2019,7,30) ),
 
 ]
 toogleDetails(index){
